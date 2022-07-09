7b7b32264e1b2b245d67cf439aa1a1c0412ca4ce
Carlos Diones <carlosdiones28@gmail.com>
2022-07-09 16:09:47 -0300

    Initial commit

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
     public class Movie
     {
         public int ID { get; set; }
+       
+        [Display(Name = "Título")]
         public string Title { get; set; }
 
+        [Display(Name = "Data de estreia")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+       
+        [Display(Name = "Gênero")]
         public string Genre { get; set; }
+
+        [Display(Name = "Preço")]
+        [Column (TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
---
+++ Models/SeedData.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Data;
+using System;
+using System.Linq;
+
+namespace RazorPagesMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesMovieContext(
+                serviceProvider.GetRequiredService<DbContextOptions<RazorPagesMovieContext>>()))
+            {
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Bee Movie",
+                        ReleaseDate = DateTime.Parse("05-10-2005"),
+                        Genre = "Comédia",
+                        Price = 71M
+                    },
+
+                    new Movie
+                    {
+                        Title = "El Dourado",
+                        ReleaseDate = DateTime.Parse("10-05-2002"),
+                        Genre = "Aventura",
+                        Price = 40M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Liga da Justiça",
+                        ReleaseDate = DateTime.Parse("16-05-2004"),
+                        Genre = "Heróis",
+                        Price = 59M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Norbit",
+                        ReleaseDate = DateTime.Parse("14-07-2008"),
+                        Genre = "Comédia",
+                        Price = 92M
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
